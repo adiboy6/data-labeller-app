f89ab58bfa9163d63bd8162bed9d5c264fed015b
adiboy6 <aditya.reddy.97@gmail.com>
2026-03-23 17:44:06 -0400

    reverted changes

--- env.mjs
+++ env.mjs
@@ -1,32 +1,67 @@
 import { createEnv } from "@t3-oss/env-nextjs"
 import { z } from "zod"
 
-const skip = !!process.env.SKIP_ENV_VALIDATION
-
-const optional = (schema) => (skip ? schema.optional() : schema)
-
 export const env = createEnv({
   server: {
-    ADMIN_API_KEY: optional(z.string().min(1)),
-    POSTGRES_PRISMA_URL: optional(z.string().min(1)),
-    POSTGRES_URL_NON_POOLING: optional(z.string().min(1)),
+    // This is optional because it's only used in development.
+    // See https://next-auth.js.org/deployment.
+    // NEXTAUTH_URL: z.string().url().optional(),
+    // NEXTAUTH_SECRET: z.string().min(1),
+    // GITHUB_CLIENT_ID: z.string().min(1),
+    // GITHUB_CLIENT_SECRET: z.string().min(1),
+    // GITHUB_ACCESS_TOKEN: z.string().min(1),
+    // DATABASE_URL: z.string().min(1),
+    // SMTP_FROM: z.string().min(1),
+    // POSTMARK_API_TOKEN: z.string().min(1),
+    // POSTMARK_SIGN_IN_TEMPLATE: z.string().min(1),
+    // POSTMARK_ACTIVATION_TEMPLATE: z.string().min(1),
+    // STRIPE_API_KEY: z.string().min(1),
+    // STRIPE_WEBHOOK_SECRET: z.string().min(1),
+    // STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
+    // GOOGLE_CLIENT_ID: z.string().min(1),
+    // GOOGLE_CLIENT_SECRET: z.string().min(1),
+    ADMIN_API_KEY: z.string().min(1),
+    // POSTGRES_URL: z.string().min(1),
+    POSTGRES_PRISMA_URL: z.string().min(1),
+    // POSTGRES_URL_NO_SSL: z.string().min(1),
+    POSTGRES_URL_NON_POOLING: z.string().min(1),
+    // POSTGRES_USER: z.string().min(1),
+    // POSTGRES_HOST: z.string().min(1),
+    // POSTGRES_PASSWORD: z.string().min(1),
+    // POSTGRES_DATABASE: z.string().min(1),
   },
   client: {
-    NEXT_PUBLIC_APP_URL: optional(z.string().min(1)),
+    // RECAPTCHA_SECRET_KEY: z.string().min(1),
+    // RECAPTCHA_SITE_KEY: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
+    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
+    // DATABASE_URL: process.env.DATABASE_URL,
     SMTP_FROM: process.env.SMTP_FROM,
     POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
     POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,
     POSTMARK_ACTIVATION_TEMPLATE: process.env.POSTMARK_ACTIVATION_TEMPLATE,
+    // STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
+    // STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     ADMIN_API_KEY: process.env.ADMIN_API_KEY,
+    // RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+    // RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
     POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
     POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
+    // POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
+    // POSTGRES_USER: process.env.POSTGRES_USER,
+    // POSTGRES_HOST: process.env.POSTGRES_HOST,
+    // POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    // POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
+    // POSTGRES_URL: process.env.POSTGRES_URL,
   },
 })
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import { withContentlayer } from "next-contentlayer"
+
+import "./env.mjs"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -9,4 +13,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withContentlayer(nextConfig)
